20ea262d65e003f655484891547f3356f37f51a4
Aswathi K E <aswathi.ke25@gmail.com>
2025-12-26 15:11:25 +0530

    Update OrganizationTest.java

--- src/test/java/organization_module/OrganizationTest.java
+++ src/test/java/organization_module/OrganizationTest.java
@@ -1,8 +1,9 @@
-package organization_module;
+epackage organization_module;
 
 import genericUtils.BaseClass;
 
 public class OrganizationTest extends BaseClass{
 	
-
+hi i made changes to your orgclass in github
 }
+
