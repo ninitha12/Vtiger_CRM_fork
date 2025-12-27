67c0fa6e86cdc8919943fbbc8cdb8b8f48d23c25
Sharon <Sharon@LAPTOP-1DLOQGU3>
2025-12-27 12:19:25 +0530

    created lead module

---
+++ src/test/java/lead_module/Create_lead.java
@@ -0,0 +1,11 @@
+package lead_module;
+
+import org.testng.annotations.Test;
+
+public class Create_lead {
+	@Test
+	public void creating_lead() {
+		System.out.println("--this is create lead module--");
+	}
+
+}
