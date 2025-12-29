73a05e78c07ea0d5f362034db8060fc202996336
Sharon <Sharon@LAPTOP-1DLOQGU3>
2025-12-29 12:57:46 +0530

    added changes

--- src/test/java/contacts_module/ContactTest.java
+++ src/test/java/contacts_module/ContactTest.java
@@ -11,4 +11,8 @@ public class ContactTest extends BaseClass{
 	public void contTest() {
 		System.out.println("----cont test----");
 	}
+	@Test
+	public void contTest2() {
+		System.out.println("----cont test----");
+	}
 }
--- src/test/java/login_module/LoginTest.java
+++ src/test/java/login_module/LoginTest.java
@@ -1,9 +1,15 @@
 package login_module;
 
 
+import org.testng.annotations.Test;
+
 import genericUtils.BaseClass;
 
 
 public class LoginTest extends BaseClass{
+	@Test
+	public void logintest1() {
+		System.out.println("---this is login---");
+	}
 
 }
